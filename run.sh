ee1f325254ca2a49f496c80465742a587992401e
ufian <ufian@ya.ru>
2021-05-01 21:24:55 -0700

    v1.0.21 python2

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./create_repository.py --datadir=. https://github.com/Soap4me/Kodi.git: https://github.com/eschava/soap4me-proxy.git:
+./create_repository.py --datadir=. https://github.com/Soap4me/Kodi.git: https://github.com/Soap4me/Kodi.git#py2: https://github.com/eschava/soap4me-proxy.git:
